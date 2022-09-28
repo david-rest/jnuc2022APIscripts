0fc0a8e02ad578d8dd38d08d797de2d3601837e2
david-rest <111327318+david-rest@users.noreply.github.com>
2022-09-28 14:05:58 -0500

    Add files via upload

---
+++ jnuc2022GetToken.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#hardcode our Jamf Pro URL for the script
+jamfProURL=""
+
+#optionally hardcode the Jamf Pro username and password
+jamfProUser=""
+jamfProPassword=""
+
+#if we didn't hardcode the Jamf Pro username, prompt the user for the Jamf Pro username
+if [[ -z $jamfProUser ]]; then
+	read -p "Please enter your Jamf Pro user account : " jamfProUser
+fi
+
+#if we didn't hardcode the Jamf Pro password, prompt the user for the Jamf Pro password
+if [[ -z $jamfProPassword ]]; then
+	read -p "Please enter your Jamf Pro password : " -s jamfProPassword
+fi
+
+#generate a Bearer Token and set it as a variable
+bearerTokenFull=$(/usr/bin/curl -s -u "$jamfProUser:$jamfProPassword" "$jamfProURL/api/v1/auth/token" -X POST)
+
+#parse the json with JavaScript to get just the token
+#https://richard-purves.com/2021/12/09/jamf-pro-api-for-fun-and-profit/
+bearerToken=$(/usr/bin/osascript -l 'JavaScript' -e "JSON.parse(\`$bearerTokenFull\`).token")
+
+echo ""
+
+echo "$bearerToken"
