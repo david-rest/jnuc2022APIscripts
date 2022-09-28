604f005fd8e79a2a67b15c9134b4f7247ae3fd65
david-rest <111327318+david-rest@users.noreply.github.com>
2022-09-28 13:54:30 -0500

    Add files via upload

---
+++ jnuc2022GetSites.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#hardcode our Jamf Pro URL for the script
+jamfProURL=""
+
+#optionally hardcode the Jamf Pro username and password
+jamfProUser=""
+jamfProPassword=""
+
+#if we didn't hardcode the Jamf Pro username, prompt the user for the Jamf Pro username
+if [[ -z $jamfProUser ]]; then
+	read -p "Please enter your Jamf Pro user account : " jamfProUser
+fi
+
+#if we didn't hardcode the Jamf Pro paswroed, prompt the user for the Jamf Pro password
+if [[ -z $jamfProPassword ]]; then
+	read -p "Please enter your Jamf Pro password : " -s jamfProPassword
+fi
+
+#generate a Bearer Token and set it as a variable
+bearerTokenFull=$(/usr/bin/curl -s -u "$jamfProUser:$jamfProPassword" "$jamfProURL/api/v1/auth/token" -X POST)
+
+#parse the json with JavaScript to get just the token
+#https://richard-purves.com/2021/12/09/jamf-pro-api-for-fun-and-profit/
+bearerToken=$(/usr/bin/osascript -l 'JavaScript' -e "JSON.parse(\`$bearerTokenFull\`).token")
+
+echo ""
+
+#GET a list of sites
+/usr/bin/curl -s \
+-H "Authorization: Bearer $bearerToken" \
+-H "accept: application/xml" \
+"$jamfProURL"/JSSResource/sites \
+-X GET \
+| xmllint --format -
